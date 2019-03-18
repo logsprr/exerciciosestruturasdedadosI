688be5736572d87139d23b211c886e864a3668c2
Gabriel Gomes <corinthiaswwyy@gmail.com>
2019-03-17 22:15:49 -0300

    Exercicio 1d lista 3 fluindo

--- Lista 3/EX1C.c
+++ Lista 3/EX1C.c
@@ -1,7 +1,51 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+struct cliente{
+    char nome[50];
+    float debitos = 15; //juros
+    float saldo;
 
+};
+void cadastra(){
+    struct cliente Cliente;
+    printf("Digite o nome do Cliente:\n");
+    fgets(Cliente.nome,sizeof(nome),stdin);
+    printf("Quanto vai depositar ? \n");
+    scanf("%f",&Cliente.saldo);
+}
+void vecliente(){
+    printf("Digite o codigo de sua conta:\n");
+    int i;
+    int op;
+    scanf("%d",&op);
+    for(i=0;i<5;i++){
+        if(numconta == op){
+            printf("NOme :");
+            printf("Saldo : ");
+            printf("DEbitos :");
+        }
+    }
+}
+void menu(){
+    int op;
+    do{
+        printf("Cadastrar Cliente: 1");
+        printf("Ver  Cliente: 2");
+        printf("Sair: 3");
+        scanf("%d",&op);
+        getchar();
+    }while(op!=3);
+    switch(op){
+        case 1:
+            cadastra();
+            break;
+        case 2:
+            vecliente();
+        default : printf("Opcao errada:\n");
+    }
+
+}
 int main()
 {
 
@@ -11,7 +55,6 @@ int main()
     float credito;
     int senha1[5]= {{1234},{2345},{3456},{4567},{5678}};
     int senha[5];
-
 
 
     printf("Digite o numero da sua conta: \n");
@@ -50,8 +93,8 @@ int main()
    int i;
 
    for(i=0;i<5;i++){
-        if(senha1i]=senha[i]){
-
+        if(senha1[i]=senha[i]){
+            printf("")
         }
 
    }
